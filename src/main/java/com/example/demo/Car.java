b3e49cd7f49a5ee39c1e1d84baf067210fbb8c03
Łukasz Macoń <luk.macon@gmail.com>
2021-04-18 15:00:15 +0200

    New clas response

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Car {
+    private String make;
+
+    public Car(String make) {
+        this.make = make;
+    }
+
+    public Car (){
+
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String field) {
+        this.make = make;
+    }
+}
--- src/main/java/com/example/demo/SandboxController.java
+++ src/main/java/com/example/demo/SandboxController.java
@@ -1,15 +1,23 @@
 package com.example.demo;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sandbox")
+@RequestMapping("/test")
 public class SandboxController {
+
     @GetMapping("/hello")
-    public ResponseEntity<String> helloWorld(){
+    public ResponseEntity<String> getHelloWorld(){
         return ResponseEntity.ok("Hello world");
+    }
+
+    @GetMapping("/model")
+    public ResponseEntity<Car> getCarModel(){
+        Car car = new Car("Audi");
+        return ResponseEntity.ok(car);
     }
 }
