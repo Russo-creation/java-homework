bf4a24e9eeeed38a78234da12290cf9a7b0c4e22
Łukasz Macoń <luk.macon@gmail.com>
2021-04-18 15:33:50 +0200

    Get params

--- src/main/java/com/example/demo/SandboxController.java
+++ src/main/java/com/example/demo/SandboxController.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -20,4 +19,15 @@ public class SandboxController {
         Car car = new Car("Audi");
         return ResponseEntity.ok(car);
     }
+
+    @GetMapping("/hello/{someValue}")
+    public ResponseEntity<String> getPathVariable(@PathVariable String someValue) {
+        return ResponseEntity.ok(someValue);
+    }
+
+    @GetMapping("/hello/")
+    public ResponseEntity<String> getReqVariable(@RequestParam String reqParam) {
+        return ResponseEntity.ok(reqParam);
+    }
+
 }
